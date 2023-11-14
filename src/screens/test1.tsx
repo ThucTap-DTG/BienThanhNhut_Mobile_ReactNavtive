62fbf61457f028b4216524a46c56f887a142243b
bthanhnhut <bthanhnhut10@gmail.com>
2023-11-15 00:18:48 +0700

    update test 1

--- src/screens/test1.tsx
+++ src/screens/test1.tsx
@@ -1,18 +1,41 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { View, Button, Platform } from "react-native";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
-const Home = () => {
+const MyDateTimePicker: React.FC = () => {
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || date;
+    setShowPicker(Platform.OS === "ios"); // On iOS, always show picker
+    setDate(currentDate);
+    // You can perform actions with the selected date here
+  };
+
+  const showDatepicker = () => {
+    setShowPicker(true);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text style={{ fontSize: 30 }}>test1</Text>
+    <View>
+      <View>
+        <Button onPress={showDatepicker} title="Show Date Picker" />
+      </View>
+      {showPicker && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={date}
+          mode="date" // You can set mode as "time" for time picker
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
 
-export default Home;
+export default MyDateTimePicker;
