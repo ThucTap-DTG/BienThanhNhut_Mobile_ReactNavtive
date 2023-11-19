7240362d052bb80e20a1122e82ad19ec69da532f
bthanhnhut <bthanhnhut10@gmail.com>
2023-11-20 00:06:04 +0700

    commit

--- src/screens/test1.tsx
+++ src/screens/test1.tsx
@@ -1,50 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import { View, TextInput, Platform } from "react-native";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
-const InfiniteScrollComponent = () => {
-  const [data, setData] = useState([]); // Dữ liệu để hiển thị
-  const [page, setPage] = useState(1); // Số trang hiện tại
-  const [loading, setLoading] = useState(false); // Trạng thái loading
+const formatDate = (date: Date): string => {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  return `${day < 10 ? "0" + day : day}/${
+    month < 10 ? "0" + month : month
+  }/${year}`;
+};
 
-  useEffect(() => {
-    // Hàm để tải dữ liệu mới khi cuộn trang
-    const loadMoreData = async () => {
-      setLoading(true);
-      // Thực hiện logic để tải dữ liệu từ API hoặc nguồn dữ liệu khác
-      // Ở đây, chúng ta giả định fetchData là một hàm lấy dữ liệu từ API
-      const newData = await fetchData(page);
-      setData((prevData) => [...prevData, ...newData]); // Cập nhật dữ liệu mới
-      setLoading(false);
-    };
+const MyDateTimePicker: React.FC = () => {
+  const [date1, setDate1] = useState(new Date());
+  const [date2, setDate2] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const [focusedInput, setFocusedInput] = useState("");
 
-    const handleScroll = () => {
-      // Kiểm tra xem người dùng đã cuộn đến cuối trang chưa
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        // Tải dữ liệu mới khi cuộn đến cuối trang
-        if (!loading) {
-          setPage((prevPage) => prevPage + 1); // Tăng số trang lên
-        }
-      }
-    };
+  const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
+    const currentDate = selectedDate || new Date();
+    setShowPicker(Platform.OS === "ios");
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [page, loading]);
+    if (focusedInput === "input1") {
+      setDate1(currentDate); 
+    } else if (focusedInput === "input2") {
+      setDate2(currentDate);
+    }
+  };
+
+  const showDatepicker = (input: string) => {
+    setFocusedInput(input);
+    setShowPicker(true);
+  };
 
   return (
-    <div>
-      {/* Hiển thị dữ liệu */}
-      {data.map((item) => (
-        <div key={item.id}>{/* Hiển thị item */}</div>
-      ))}
-      {/* Hiển thị loading nếu đang tải */}
-      {loading && <p>Loading...</p>}
-    </div>
+    <View>
+      <TextInput
+        value={formatDate(date1)}
+        placeholder="Select Date 1"
+        style={{ borderWidth: 1, padding: 10 }}
+        onPressIn={() => showDatepicker("input1")}
+      />
+      <TextInput
+        value={formatDate(date2)}
+        placeholder="Select Date 2"
+        style={{ borderWidth: 1, padding: 10 }}
+        onPressIn={() => showDatepicker("input2")}
+      />
+      {showPicker && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={focusedInput === "input1" ? date1 : date2}
+          mode="date"
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
+    </View>
   );
 };
 
-export default InfiniteScrollComponent;
+export default MyDateTimePicker;
